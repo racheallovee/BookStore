7e30bdc7a2069b2115909611597ec80d55caadd9
racheallovee <rachealloveo6@gmail.com>
2024-12-10 22:42:14 +0300

    added a new contract

---
+++ contracts/IntWrapAround.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: GPL-3.0
+ pragma solidity ^0.8.7;
+
+
+ contract WrapAround{
+    uint8 public variable;
+
+    function increment() public{
+        variable++ ;
+    }
+
+    function decrement() public{
+        variable-- ;
+    }
+ }
+
+

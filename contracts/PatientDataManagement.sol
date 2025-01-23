5b392c19782970e2e3851b669938cc32b9d72659
racheallovee <rachealloveo6@gmail.com>
2025-01-23 23:44:08 +0300

    modified the contract

--- contracts/PatientDataManagement.sol
+++ contracts/PatientDataManagement.sol
@@ -2,7 +2,6 @@
 pragma solidity ^0.8.0;
 
 contract PatientDataManagement {
-    // Structure to store patient health data
     struct HealthData {
         uint256 timestamp;
         uint256 glucoseLevel;
@@ -11,19 +10,14 @@ contract PatientDataManagement {
         string exercise;
     }
 
-    // Mapping to store health data for each patient (address)
     mapping(address => HealthData[]) private patientData;
 
-    // Mapping to track authorized researchers/providers for data access
     mapping(address => mapping(address => bool)) private accessPermissions;
 
-    // Event emitted when health data is logged
     event DataLogged(address indexed patient, uint256 timestamp);
 
-    // Event emitted when access is granted or revoked
     event AccessUpdated(address indexed patient, address indexed accessor, bool isGranted);
 
-    // Function for patients to log their health data
     function logHealthData(
         uint256 _glucoseLevel,
         string memory _medication,
@@ -41,13 +35,11 @@ contract PatientDataManagement {
         emit DataLogged(msg.sender, block.timestamp);
     }
 
-    // Function for patients to grant or revoke access to their data
     function updateAccess(address _accessor, bool _isGranted) public {
         accessPermissions[msg.sender][_accessor] = _isGranted;
         emit AccessUpdated(msg.sender, _accessor, _isGranted);
     }
 
-    // Function for authorized users to view a patient's health data
     function viewHealthData(address _patient) public view returns (HealthData[] memory) {
         require(
             _patient == msg.sender || accessPermissions[_patient][msg.sender],
